ad3a84d494244c23edb37398ccb8592413f2af52
TodorHristov06 <todorhristov2006@gmail.com>
2024-04-25 17:30:14 +0300

    moved button for aesthetic purpose

---
+++ FirefighterProject/View/MainView.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FirefighterProject.View
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.pblUpper = new System.Windows.Forms.Panel();
+            this.btnDash = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.pbFireLogo = new System.Windows.Forms.PictureBox();
+            this.pnlLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFireLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.BackColor = System.Drawing.Color.White;
+            this.pnlLeft.Controls.Add(this.pbFireLogo);
+            this.pnlLeft.Controls.Add(this.btnDelete);
+            this.pnlLeft.Controls.Add(this.btnUpdate);
+            this.pnlLeft.Controls.Add(this.btnAdd);
+            this.pnlLeft.Controls.Add(this.btnDash);
+            this.pnlLeft.Location = new System.Drawing.Point(1, -2);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(358, 649);
+            this.pnlLeft.TabIndex = 0;
+            // 
+            // pblUpper
+            // 
+            this.pblUpper.BackColor = System.Drawing.Color.RosyBrown;
+            this.pblUpper.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pblUpper.Location = new System.Drawing.Point(0, 0);
+            this.pblUpper.Name = "pblUpper";
+            this.pblUpper.Size = new System.Drawing.Size(1286, 40);
+            this.pblUpper.TabIndex = 1;
+            // 
+            // btnDash
+            // 
+            this.btnDash.BackColor = System.Drawing.Color.IndianRed;
+            this.btnDash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDash.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDash.ForeColor = System.Drawing.Color.White;
+            this.btnDash.Location = new System.Drawing.Point(72, 239);
+            this.btnDash.Name = "btnDash";
+            this.btnDash.Size = new System.Drawing.Size(215, 41);
+            this.btnDash.TabIndex = 2;
+            this.btnDash.Text = "DASHBOARD";
+            this.btnDash.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.IndianRed;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(72, 304);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(215, 41);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.IndianRed;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(72, 369);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(215, 41);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.IndianRed;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(72, 436);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(215, 41);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // pbFireLogo
+            // 
+            this.pbFireLogo.Image = global::FirefighterProject.Properties.Resources.finalFireman1;
+            this.pbFireLogo.Location = new System.Drawing.Point(94, 48);
+            this.pbFireLogo.Name = "pbFireLogo";
+            this.pbFireLogo.Size = new System.Drawing.Size(183, 150);
+            this.pbFireLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFireLogo.TabIndex = 6;
+            this.pbFireLogo.TabStop = false;
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(1286, 639);
+            this.Controls.Add(this.pblUpper);
+            this.Controls.Add(this.pnlLeft);
+            this.Name = "MainView";
+            this.Text = "MainView";
+            this.pnlLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFireLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLeft;
+        private System.Windows.Forms.Panel pblUpper;
+        private System.Windows.Forms.Button btnDash;
+        private System.Windows.Forms.PictureBox pbFireLogo;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
