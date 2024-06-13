214244fc261939618caa326417b6c348a3d00434
Eray Nejdet <fivebrodhers@gmail.com>
2024-06-13 18:44:40 +0300

    .

--- FirefighterProject/View/MainView.Designer.cs
+++ FirefighterProject/View/MainView.Designer.cs
@@ -25,17 +25,15 @@
             this.btnDash = new System.Windows.Forms.Button();
             this.btnAddIncident = new System.Windows.Forms.Button();
             this.btnViewIncidentDetails = new System.Windows.Forms.Button();
+            this.btnAddFiretruck = new System.Windows.Forms.Button();
+            this.btnAssignFirefightersToFiretruck = new System.Windows.Forms.Button();
+            this.btnTopFirefighters = new System.Windows.Forms.Button();
+            this.btnTopFiretrucks = new System.Windows.Forms.Button();
             this.pblUpper = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridViewIncidents = new System.Windows.Forms.DataGridView();
-            this.btnSortFirefighters = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.pnlLeft.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
-            this.pblUpper.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIncidents)).BeginInit();
             this.SuspendLayout();
@@ -43,10 +41,6 @@
             // pnlLeft
             // 
             this.pnlLeft.BackColor = System.Drawing.Color.White;
-            this.pnlLeft.Controls.Add(this.panel4);
-            this.pnlLeft.Controls.Add(this.panel3);
-            this.pnlLeft.Controls.Add(this.panel2);
-            this.pnlLeft.Controls.Add(this.btnSortFirefighters);
             this.pnlLeft.Controls.Add(this.pbLogo);
             this.pnlLeft.Controls.Add(this.btnDelete);
             this.pnlLeft.Controls.Add(this.btnUpdate);
@@ -54,16 +48,20 @@
             this.pnlLeft.Controls.Add(this.btnDash);
             this.pnlLeft.Controls.Add(this.btnAddIncident);
             this.pnlLeft.Controls.Add(this.btnViewIncidentDetails);
+            this.pnlLeft.Controls.Add(this.btnAddFiretruck);
+            this.pnlLeft.Controls.Add(this.btnAssignFirefightersToFiretruck);
+            this.pnlLeft.Controls.Add(this.btnTopFirefighters);
+            this.pnlLeft.Controls.Add(this.btnTopFiretrucks);
             this.pnlLeft.Location = new System.Drawing.Point(1, -2);
             this.pnlLeft.Name = "pnlLeft";
-            this.pnlLeft.Size = new System.Drawing.Size(358, 649);
+            this.pnlLeft.Size = new System.Drawing.Size(407, 656);
             this.pnlLeft.TabIndex = 0;
             // 
             // pbLogo
             // 
             this.pbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pbLogo.Image = global::FirefighterProject.Properties.Resources.fmLogo;
-            this.pbLogo.Location = new System.Drawing.Point(72, 64);
+            this.pbLogo.Location = new System.Drawing.Point(82, 64);
             this.pbLogo.Name = "pbLogo";
             this.pbLogo.Size = new System.Drawing.Size(215, 162);
             this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -76,9 +74,9 @@
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDelete.ForeColor = System.Drawing.Color.White;
-            this.btnDelete.Location = new System.Drawing.Point(72, 498);
+            this.btnDelete.Location = new System.Drawing.Point(72, 512);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(215, 30);
+            this.btnDelete.Size = new System.Drawing.Size(225, 30);
             this.btnDelete.TabIndex = 5;
             this.btnDelete.Text = "DELETE";
             this.btnDelete.UseVisualStyleBackColor = false;
@@ -90,9 +88,9 @@
             this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.ForeColor = System.Drawing.Color.White;
-            this.btnUpdate.Location = new System.Drawing.Point(72, 458);
+            this.btnUpdate.Location = new System.Drawing.Point(72, 472);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(215, 34);
+            this.btnUpdate.Size = new System.Drawing.Size(225, 34);
             this.btnUpdate.TabIndex = 4;
             this.btnUpdate.Text = "UPDATE";
             this.btnUpdate.UseVisualStyleBackColor = false;
@@ -104,9 +102,9 @@
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAdd.ForeColor = System.Drawing.Color.White;
-            this.btnAdd.Location = new System.Drawing.Point(72, 418);
+            this.btnAdd.Location = new System.Drawing.Point(72, 435);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(215, 34);
+            this.btnAdd.Size = new System.Drawing.Size(225, 34);
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "ADD";
             this.btnAdd.UseVisualStyleBackColor = false;
@@ -118,9 +116,9 @@
             this.btnDash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDash.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDash.ForeColor = System.Drawing.Color.White;
-            this.btnDash.Location = new System.Drawing.Point(72, 232);
+            this.btnDash.Location = new System.Drawing.Point(72, 393);
             this.btnDash.Name = "btnDash";
-            this.btnDash.Size = new System.Drawing.Size(215, 36);
+            this.btnDash.Size = new System.Drawing.Size(225, 36);
             this.btnDash.TabIndex = 2;
             this.btnDash.Text = "DASHBOARD";
             this.btnDash.UseVisualStyleBackColor = false;
@@ -134,7 +132,7 @@
             this.btnAddIncident.ForeColor = System.Drawing.Color.White;
             this.btnAddIncident.Location = new System.Drawing.Point(72, 305);
             this.btnAddIncident.Name = "btnAddIncident";
-            this.btnAddIncident.Size = new System.Drawing.Size(215, 41);
+            this.btnAddIncident.Size = new System.Drawing.Size(225, 41);
             this.btnAddIncident.TabIndex = 7;
             this.btnAddIncident.Text = "ADD INCIDENT";
             this.btnAddIncident.UseVisualStyleBackColor = false;
@@ -148,16 +146,71 @@
             this.btnViewIncidentDetails.ForeColor = System.Drawing.Color.White;
             this.btnViewIncidentDetails.Location = new System.Drawing.Point(72, 352);
             this.btnViewIncidentDetails.Name = "btnViewIncidentDetails";
-            this.btnViewIncidentDetails.Size = new System.Drawing.Size(215, 35);
+            this.btnViewIncidentDetails.Size = new System.Drawing.Size(225, 35);
             this.btnViewIncidentDetails.TabIndex = 8;
             this.btnViewIncidentDetails.Text = "VIEW INCIDENT DETAILS";
             this.btnViewIncidentDetails.UseVisualStyleBackColor = false;
             this.btnViewIncidentDetails.Click += new System.EventHandler(this.btnViewIncidentDetails_Click);
             // 
+            // btnAddFiretruck
+            // 
+            this.btnAddFiretruck.BackColor = System.Drawing.Color.IndianRed;
+            this.btnAddFiretruck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddFiretruck.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddFiretruck.ForeColor = System.Drawing.Color.White;
+            this.btnAddFiretruck.Location = new System.Drawing.Point(72, 548);
+            this.btnAddFiretruck.Name = "btnAddFiretruck";
+            this.btnAddFiretruck.Size = new System.Drawing.Size(225, 30);
+            this.btnAddFiretruck.TabIndex = 9;
+            this.btnAddFiretruck.Text = "ADD FIRETRUCK";
+            this.btnAddFiretruck.UseVisualStyleBackColor = false;
+            this.btnAddFiretruck.Click += new System.EventHandler(this.btnAddFiretruck_Click);
+            // 
+            // btnAssignFirefightersToFiretruck
+            // 
+            this.btnAssignFirefightersToFiretruck.BackColor = System.Drawing.Color.IndianRed;
+            this.btnAssignFirefightersToFiretruck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAssignFirefightersToFiretruck.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAssignFirefightersToFiretruck.ForeColor = System.Drawing.Color.White;
+            this.btnAssignFirefightersToFiretruck.Location = new System.Drawing.Point(72, 584);
+            this.btnAssignFirefightersToFiretruck.Name = "btnAssignFirefightersToFiretruck";
+            this.btnAssignFirefightersToFiretruck.Size = new System.Drawing.Size(225, 35);
+            this.btnAssignFirefightersToFiretruck.TabIndex = 10;
+            this.btnAssignFirefightersToFiretruck.Text = "ASSIGN FIREFIGHTERS";
+            this.btnAssignFirefightersToFiretruck.UseVisualStyleBackColor = false;
+            this.btnAssignFirefightersToFiretruck.Click += new System.EventHandler(this.btnAssignFirefightersToFiretruck_Click);
+            // 
+            // btnTopFirefighters
+            // 
+            this.btnTopFirefighters.BackColor = System.Drawing.Color.IndianRed;
+            this.btnTopFirefighters.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTopFirefighters.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTopFirefighters.ForeColor = System.Drawing.Color.White;
+            this.btnTopFirefighters.Location = new System.Drawing.Point(23, 230);
+            this.btnTopFirefighters.Name = "btnTopFirefighters";
+            this.btnTopFirefighters.Size = new System.Drawing.Size(144, 52);
+            this.btnTopFirefighters.TabIndex = 11;
+            this.btnTopFirefighters.Text = "TOP 3 FIREFIGHTERS";
+            this.btnTopFirefighters.UseVisualStyleBackColor = false;
+            this.btnTopFirefighters.Click += new System.EventHandler(this.btnViewTopFirefighters_Click);
+            // 
+            // btnTopFiretrucks
+            // 
+            this.btnTopFiretrucks.BackColor = System.Drawing.Color.IndianRed;
+            this.btnTopFiretrucks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTopFiretrucks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTopFiretrucks.ForeColor = System.Drawing.Color.White;
+            this.btnTopFiretrucks.Location = new System.Drawing.Point(227, 232);
+            this.btnTopFiretrucks.Name = "btnTopFiretrucks";
+            this.btnTopFiretrucks.Size = new System.Drawing.Size(141, 50);
+            this.btnTopFiretrucks.TabIndex = 12;
+            this.btnTopFiretrucks.Text = "TOP 3 FIRETRUCKS";
+            this.btnTopFiretrucks.UseVisualStyleBackColor = false;
+            this.btnTopFiretrucks.Click += new System.EventHandler(this.btnViewTopFiretrucks_Click);
+            // 
             // pblUpper
             // 
             this.pblUpper.BackColor = System.Drawing.Color.RosyBrown;
-            this.pblUpper.Controls.Add(this.panel1);
             this.pblUpper.Dock = System.Windows.Forms.DockStyle.Top;
             this.pblUpper.Location = new System.Drawing.Point(0, 0);
             this.pblUpper.Name = "pblUpper";
@@ -182,53 +235,6 @@
             this.dataGridViewIncidents.TabIndex = 9;
             this.dataGridViewIncidents.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewIncidents_CellContentClick);
             // 
-            // btnSortFirefighters
-            // 
-            this.btnSortFirefighters.BackColor = System.Drawing.Color.IndianRed;
-            this.btnSortFirefighters.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSortFirefighters.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSortFirefighters.ForeColor = System.Drawing.Color.White;
-            this.btnSortFirefighters.Location = new System.Drawing.Point(72, 555);
-            this.btnSortFirefighters.Name = "btnSortFirefighters";
-            this.btnSortFirefighters.Size = new System.Drawing.Size(215, 41);
-            this.btnSortFirefighters.TabIndex = 13;
-            this.btnSortFirefighters.Text = "SORT";
-            this.btnSortFirefighters.UseVisualStyleBackColor = false;
-            this.btnSortFirefighters.Click += new System.EventHandler(this.btnSortFirefighters_Click);
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.RosyBrown;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1307, 40);
-            this.panel1.TabIndex = 2;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.LightGray;
-            this.panel2.Location = new System.Drawing.Point(27, 274);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(316, 10);
-            this.panel2.TabIndex = 10;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.LightGray;
-            this.panel3.Location = new System.Drawing.Point(27, 393);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(316, 10);
-            this.panel3.TabIndex = 11;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.LightGray;
-            this.panel4.Location = new System.Drawing.Point(27, 534);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(316, 10);
-            this.panel4.TabIndex = 11;
-            // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -243,7 +249,6 @@
             this.Text = "MainView";
             this.pnlLeft.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
-            this.pblUpper.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIncidents)).EndInit();
             this.ResumeLayout(false);
@@ -263,10 +268,9 @@
         private System.Windows.Forms.Button btnAddIncident;
         private System.Windows.Forms.Button btnViewIncidentDetails;
         private System.Windows.Forms.DataGridView dataGridViewIncidents;
-        private System.Windows.Forms.Button btnSortFirefighters;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAddFiretruck;
+        private System.Windows.Forms.Button btnAssignFirefightersToFiretruck;
+        private System.Windows.Forms.Button btnTopFirefighters;
+        private System.Windows.Forms.Button btnTopFiretrucks;
     }
 }
--- FirefighterProject/View/MainView.cs
+++ FirefighterProject/View/MainView.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Windows.Forms;
 using FirefighterProject.Controller;
 using FirefighterProject.Data;
+using FirefighterProject.Model;
 
 namespace FirefighterProject.View
 {
@@ -17,7 +18,6 @@ namespace FirefighterProject.View
             LoadDataGrid();
             LoadIncidentDataGrid();
         }
-
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -117,22 +117,10 @@ namespace FirefighterProject.View
         private void btnDash_Click_1(object sender, EventArgs e)
         {
             _controller.LoadDashboard();
-            MessageBox.Show("Dashboard data loaded");
         }
 
-        private void btnAddIncident_Click(object sender, EventArgs e)
+        private void btnViewIncidents_Click(object sender, EventArgs e)
         {
-            var date = DateTime.Parse(Prompt.ShowDialog("Enter Date (yyyy-mm-dd) :", "Add Incident"));
-            var duration = TimeSpan.Parse(Prompt.ShowDialog("Enter Duration (hh:mm:ss) :", "Add Incident"));
-            var waterUsed = decimal.Parse(Prompt.ShowDialog("Enter Water Used  (liters) :", "Add Incident"));
-            var firetruckID = int.Parse(Prompt.ShowDialog("Enter Firetruck ID:", "Add Incident"));
-            var firefighterIDs = Prompt.ShowDialog("Enter Firefighter IDs  (comma separated) :", "Add Incident")
-                .Split(',')
-                .Select(int.Parse)
-                .ToArray();
-
-            _controller.AddIncident(date, duration, waterUsed, firetruckID, firefighterIDs);
-            MessageBox.Show("Incident added successfully");
             LoadIncidentDataGrid();
         }
 
@@ -142,7 +130,12 @@ namespace FirefighterProject.View
             {
                 var incidentID = (int)dataGridViewIncidents.SelectedRows[0].Cells["IncidentID"].Value;
                 var details = _controller.GetIncidentDetails(incidentID);
-                MessageBox.Show(details.ToString());
+
+                var detailString = details == null
+                    ? "Incident not found"
+                    : $"IncidentID = {details.IncidentID}, Date = {details.Date}, Duration = {details.Duration}, WaterUsed = {details.WaterUsed}, FiretruckID = {details.FiretruckID}, Firefighters = {string.Join(", ", details.Firefighters)}";
+
+                MessageBox.Show(detailString);
             }
             else
             {
@@ -150,56 +143,74 @@ namespace FirefighterProject.View
             }
         }
 
+        private void btnViewTopFirefighters_Click(object sender, EventArgs e)
+        {
+            var topFirefighters = _controller.GetTopFirefighters(5);
+            var topFirefightersString = string.Join(Environment.NewLine, topFirefighters.Select(f => $"Firefighter: {f.Firefighter}, IncidentCount: {f.IncidentCount}"));
+            MessageBox.Show(topFirefightersString);
+        }
+
+        private void btnViewTopFiretrucks_Click(object sender, EventArgs e)
+        {
+            var topFiretrucks = _controller.GetTopFiretrucks(5);
+            var topFiretrucksString = string.Join(Environment.NewLine, topFiretrucks.Select(t => $"FiretruckID: {t.FiretruckID}, IncidentCount: {t.IncidentCount}"));
+            MessageBox.Show(topFiretrucksString);
+        }
+
+        private void btnAddIncident_Click(object sender, EventArgs e)
+        {
+            var date = DateTime.Parse(Prompt.ShowDialog("Enter Incident Date:", "Add Incident"));
+            var duration = TimeSpan.Parse(Prompt.ShowDialog("Enter Incident Duration:", "Add Incident"));
+            var waterUsed = decimal.Parse(Prompt.ShowDialog("Enter Water Used:", "Add Incident"));
+            var firetruckID = int.Parse(Prompt.ShowDialog("Enter Firetruck ID:", "Add Incident"));
+            var firefighterIDs = Prompt.ShowDialog("Enter Firefighter IDs (comma separated):", "Add Incident")
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            _controller.AddIncident(date, duration, waterUsed, firetruckID, firefighterIDs);
+            MessageBox.Show("Incident added successfully");
+            LoadIncidentDataGrid();
+        }
+
         private void btnAddFiretruck_Click(object sender, EventArgs e)
         {
-            var isMondayShift = bool.Parse(Prompt.ShowDialog("Is Monday Shift? (true/false):", "Add Firetruck"));
-            var isTuesdayShift = bool.Parse(Prompt.ShowDialog("Is Tuesday Shift? (true/false):", "Add Firetruck"));
-            var isWednesdayShift = bool.Parse(Prompt.ShowDialog("Is Wednesday Shift? (true/false):", "Add Firetruck"));
-            var isThursdayShift = bool.Parse(Prompt.ShowDialog("Is Thursday Shift? (true/false):", "Add Firetruck"));
-            var isFridayShift = bool.Parse(Prompt.ShowDialog("Is Friday Shift? (true/false):", "Add Firetruck"));
-            var isSaturdayShift = bool.Parse(Prompt.ShowDialog("Is Saturday Shift? (true/false):", "Add Firetruck"));
-            var isSundayShift = bool.Parse(Prompt.ShowDialog("Is Sunday Shift? (true/false):", "Add Firetruck"));
+            var isMondayShift = bool.Parse(Prompt.ShowDialog("Is Monday Shift?", "Add Firetruck"));
+            var isTuesdayShift = bool.Parse(Prompt.ShowDialog("Is Tuesday Shift?", "Add Firetruck"));
+            var isWednesdayShift = bool.Parse(Prompt.ShowDialog("Is Wednesday Shift?", "Add Firetruck"));
+            var isThursdayShift = bool.Parse(Prompt.ShowDialog("Is Thursday Shift?", "Add Firetruck"));
+            var isFridayShift = bool.Parse(Prompt.ShowDialog("Is Friday Shift?", "Add Firetruck"));
+            var isSaturdayShift = bool.Parse(Prompt.ShowDialog("Is Saturday Shift?", "Add Firetruck"));
+            var isSundayShift = bool.Parse(Prompt.ShowDialog("Is Sunday Shift?", "Add Firetruck"));
 
             if (_controller.AddFiretruck(isMondayShift, isTuesdayShift, isWednesdayShift, isThursdayShift, isFridayShift, isSaturdayShift, isSundayShift))
             {
                 MessageBox.Show("Firetruck added successfully");
+                LoadIncidentDataGrid();
             }
             else
             {
-                MessageBox.Show("Error adding firetruck");
+                MessageBox.Show("Failed to add firetruck");
             }
         }
 
         private void btnAssignFirefightersToFiretruck_Click(object sender, EventArgs e)
         {
-            var firetruckID = int.Parse(Prompt.ShowDialog("Enter Firetruck ID:", "Assign Firefighters to Firetruck"));
-            var firefighterIDs = Prompt.ShowDialog("Enter Firefighter IDs  |comma separated| :", "Assign Firefighters to Firetruck")
+            var firetruckID = int.Parse(Prompt.ShowDialog("Enter Firetruck ID:", "Assign Firefighters"));
+            var firefighterIDs = Prompt.ShowDialog("Enter Firefighter IDs (comma separated):", "Assign Firefighters")
                 .Split(',')
                 .Select(int.Parse)
                 .ToArray();
 
             if (_controller.AssignFirefightersToFiretruck(firetruckID, firefighterIDs))
             {
-                MessageBox.Show("Firefighters assigned to firetruck successfully");
+                MessageBox.Show("Firefighters assigned successfully");
                 LoadDataGrid();
             }
             else
             {
-                MessageBox.Show("Error assigning firefighters to firetruck");
+                MessageBox.Show("Failed to assign firefighters");
             }
-
-        }
-        private void btnShowDialog_Click(object sender, EventArgs e)
-        {
-            string result = Prompt.ShowDialog("Title", "Default Text");
-            MessageBox.Show("Dialog Result: " + result);
-        }
-
-
-        private void btnSortFirefighters_Click(object sender, EventArgs e)
-        {
-            var sortedFirefighters = _controller.GetFirefightersByIncidentCount();
-            dataGridView1.DataSource = sortedFirefighters;
         }
     }
 }
